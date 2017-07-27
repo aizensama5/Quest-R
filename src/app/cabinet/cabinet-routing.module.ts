428100a43cbbd0b78a0aeff7ff30e929c33977b9
pastukh <pastukh@samsonos.com>
2017-07-27 13:28:58 +0300

    cabinet module created

---
+++ src/app/cabinet/cabinet-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      {path: '', component: MainComponent}
+    ])
+  ],
+  exports: [RouterModule]
+})
+export class CabinetRoutingModule {
+}
